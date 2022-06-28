691671138de3932c18545232772fa1072d9cde21
Regina Jackson <rjackson156@gmail.com>
2022-06-28 18:27:21 -0400

    update document title

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect } from 'react';
 import './App.css';
 import TodosContainer from './components/TodosContainer';
 
+
+useEffect(() => {
+  document.title = "Todo List"
+}, []);
+
 class App extends Component {
+
+
   render () {
     return (
       <div className="container">
